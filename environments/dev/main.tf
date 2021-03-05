6486c8906a78d4c26c6aa2e8dac067eab43a695f
Charles Saroka <csaroka@gmail.com>
2021-03-05 16:08:39 -0500

    Data Lake CI/CD Test 2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Data Lake CI/CD Test 1
+# Data Lake CI/CD Test 2
 
 terraform {
   required_version = ">= 0.12.0"
